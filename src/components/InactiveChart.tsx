52c55c5975178225fc8fb4bc2f561c0cbf582af5
lmcglynn <liamo.mcglynn@comcast.net>
2025-06-02 01:58:00 -0700

    added key and title for inactive chart

--- src/components/InactiveChart.tsx
+++ src/components/InactiveChart.tsx
@@ -4,10 +4,15 @@ import './InactiveChart.css';
 const InactiveChart: React.FC = () => {
 
     return (
-        <div className="flex-container">
-            <div className="item1"><img src="/inactiveY.png" height={"95%"}></img></div>
-            <div className="item2"><img src="/inactiveBody.png" height={"95%"}></img></div>
+        <div className='inactive-chart'>
+            <div className="inactive-header">Food Trucks Inactive by the 2024-2025 Academic Year</div>
+            <div className="flex-container">
+                <div className="item1"><img src="/inactiveY.png" height={"95%"}></img></div>
+                <div className="item2"><img src="/inactiveBody.png" height={"95%"}></img></div>
+            </div>
+            <img src="/inactiveKey.png" height={"80vh"}></img>
         </div>
+        
     );
 }
 
